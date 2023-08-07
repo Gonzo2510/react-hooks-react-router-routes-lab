17bd3a117ca06012ed9451316fa021a0efd6e150
jacobgonzales <jagonz@live.com>
2023-08-07 15:53:02 -0500

    complete lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie) => {
+    return(
+      <div key={movie.title}>
+        <h3>Name: {movie.title}</h3>
+        Time: {movie.time}
+        <br />
+        <br />
+        Genres:
+        <ul>{movie.genres.map((item) => <li key={item}>{item}</li>)}</ul>
+      </div>
+    )
+  })
+
+  return (
+    <>
+      <h1>Movies Page</h1>
+      {movieList}
+    </>
+  );
 }
 
 export default Movies;
